d94108dec160402c78d163ad4e26ac5911e3f26f
arhamathar <arhamathar789@gmail.com>
2021-04-04 22:48:38 +0530

    login form ui added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import Main from "./components"
+import React from "react";
+import Navbar from "./components/home/Navbar";
+import SignIn from "./components/auth/SignIn";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
-            <Main />
+            <BrowserRouter>
+                <Navbar />
+                <Switch>
+                    {/* <Route exact path="/">
+                        <Home />
+                    </Route> */}
+                    <Route exact path="/login">
+                        <SignIn />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
         </div>
     );
 }
---
+++ src/components/auth/SignIn.js
@@ -0,0 +1,49 @@
+import React, { Fragment } from "react";
+
+const SignIn = (props) => {
+    return (
+        <Fragment>
+            <div className="container mt-3">
+                <div className="col-md-6 offset-md-3 shadow p-md-5 p-sm-3 p-3">
+                    <form action="">
+                        <div>
+                            <h3 className="font-weight-light">
+                                Sign In | Blooms Bay
+                            </h3>
+                        </div>
+                        <hr />
+                        <div className="form-group">
+                            <label htmlFor="emailPhone">
+                                Email or phone number
+                            </label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                name="emailPhone"
+                                id="emailPhone"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input
+                                type="password"
+                                className="form-control"
+                                name="password"
+                                id="password"
+                            />
+                        </div>
+                        <button
+                            type="button"
+                            class="btn btn-primary btn-block mb-1"
+                        >
+                            SIGNIN
+                        </button>
+                        <a href="/signup"> Haven't account? Sign Up here </a>
+                    </form>
+                </div>
+            </div>
+        </Fragment>
+    );
+};
+
+export default SignIn;
