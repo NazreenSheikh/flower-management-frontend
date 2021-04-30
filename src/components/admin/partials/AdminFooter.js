10e0e14e5a18d75bdfc04841183f337c3871cf47
Garv <garvwashere@gmail.com>
2021-05-01 02:35:24 +0530

    fixed typos and data send names

--- src/components/admin/partials/AdminFooter.js
+++ src/components/admin/partials/AdminFooter.js
@@ -6,7 +6,7 @@ const AdminFooter = (props) => {
         <Fragment>
             <footer
                 style={{ background: '#303031', color: '#87898A' }}
-                className="z-10 py-6 px-4 md:px-12 text-center"
+                className="z-10 px-4 py-6 text-center md:px-12"
             >
                 Blooms Bay © Copyright {moment().format('YYYY')}
             </footer>
