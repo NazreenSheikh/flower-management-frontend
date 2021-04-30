e3a6c41c71990243aa3bf6e332bb66bb7632a89a
Garv <garvwashere@gmail.com>
2021-05-01 00:09:35 +0530

    implemented auth components

--- src/components/shop/auth/SignUp.js
+++ src/components/shop/auth/SignUp.js
@@ -1,86 +1,185 @@
-import React, { Fragment } from "react";
-import { Link } from "react-router-dom";
+import { useState } from 'react'
+import { signupReq } from './fetchApi'
 
 const Signup = (props) => {
-    return (
-        <Fragment>
-            <div className="container mt-3">
-                <div className="col-md-6 offset-md-3 shadow p-md-5 p-sm-3 p-3">
-                    <form action="">
-                        <div>
-                            <h3 className="font-weight-light">
-                                Sign Up | BloomsBay
-                            </h3>
-                        </div>
-                        <hr />
-                        <div className="form-group">
-                            <label htmlFor="name">Name</label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                name="name"
-                                id="name"
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="email">Email</label>
-                            <input
-                                type="email"
-                                className="form-control"
-                                name="email"
-                                id="email"
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="phone">Phone Number</label>
-                            <input
-                                type="number"
-                                className="form-control"
-                                name="phone"
-                                id="phone"
-                            />
-                        </div>
-                        {/* <div className="form-group">
-                            <label htmlFor="address">Address</label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                name="address"
-                                id="address"
-                            />
-                        </div> */}
-                        <div className="form-group">
-                            <label htmlFor="password">Password</label>
-                            <input
-                                type="password"
-                                className="form-control"
-                                name="password"
-                                id="password"
-                            />
-                        </div>
-                        {/* <div className="form-group">
-                            <label htmlFor="password">Confirm Password</label>
-                            <input
-                                type="confirmPassword"
-                                className="form-control"
-                                name="confirmPassword"
-                                id="confirmPassword"
-                            />
-                        </div> */}
-                        <button
-                            type="button"
-                            class="btn btn-dark btn-block mb-1"
-                        >
-                            SIGNUP NOW
-                        </button>
-                        <Link className="linkTag" to="/login">
-                            Have account? Sign In here
-                        </Link>
-                    </form>
-                </div>
-            </div>
-        </Fragment>
-    );
-};
+    const [data, setData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        cPassword: '',
+        error: false,
+        loading: false,
+        success: false,
+    })
 
-export default Signup;
+    const alert = (msg, type) => (
+        <div className={`text-sm text-${type}-500`}>{msg}</div>
+    )
+
+    const formSubmit = async () => {
+        setData({ ...data, loading: true })
+        if (data.cPassword !== data.password) {
+            return setData({
+                ...data,
+                error: {
+                    cPassword: "Password doesn't match",
+                    password: "Password doesn't match",
+                },
+            })
+        }
+        try {
+            let responseData = await signupReq({
+                name: data.name,
+                email: data.email,
+                password: data.password,
+            })
+            if (responseData.error) {
+                setData({
+                    ...data,
+                    loading: false,
+                    error: responseData.error,
+                    password: '',
+                    cPassword: '',
+                })
+            } else if (responseData.success) {
+                setData({
+                    success: responseData.success,
+                    name: '',
+                    email: '',
+                    password: '',
+                    cPassword: '',
+                    loading: false,
+                    error: false,
+                })
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return (
+        <>
+            <div className="mb-6 text-2xl text-center">Register</div>
+            <form className="space-y-4">
+                {data.success ? alert(data.success, 'green') : ''}
+                <div className="flex flex-col">
+                    <label htmlFor="name">
+                        Name
+                        <span className="ml-1 text-sm text-gray-600">*</span>
+                    </label>
+                    <input
+                        onChange={(e) =>
+                            setData({
+                                ...data,
+                                success: false,
+                                error: {},
+                                name: e.target.value,
+                            })
+                        }
+                        value={data.name}
+                        type="text"
+                        id="name"
+                        className={`${
+                            data.error.name ? 'border-red-500' : ''
+                        } px-4 py-2 focus:outline-none border`}
+                    />
+                    {!data.error ? '' : alert(data.error.name, 'red')}
+                </div>
+                <div className="flex flex-col">
+                    <label htmlFor="email">
+                        Email address
+                        <span className="ml-1 text-sm text-gray-600">*</span>
+                    </label>
+                    <input
+                        onChange={(e) =>
+                            setData({
+                                ...data,
+                                success: false,
+                                error: {},
+                                email: e.target.value,
+                            })
+                        }
+                        value={data.email}
+                        type="email"
+                        id="email"
+                        className={`${
+                            data.error.email ? 'border-red-500' : ''
+                        } px-4 py-2 focus:outline-none border`}
+                    />
+                    {!data.error ? '' : alert(data.error.email, 'red')}
+                </div>
+                <div className="flex flex-col">
+                    <label htmlFor="password">
+                        Password
+                        <span className="ml-1 text-sm text-gray-600">*</span>
+                    </label>
+                    <input
+                        onChange={(e) =>
+                            setData({
+                                ...data,
+                                success: false,
+                                error: {},
+                                password: e.target.value,
+                            })
+                        }
+                        value={data.password}
+                        type="password"
+                        id="password"
+                        className={`${
+                            data.error.password ? 'border-red-500' : ''
+                        } px-4 py-2 focus:outline-none border`}
+                    />
+                    {!data.error ? '' : alert(data.error.password, 'red')}
+                </div>
+                <div className="flex flex-col">
+                    <label htmlFor="cPassword">
+                        Confirm password
+                        <span className="ml-1 text-sm text-gray-600">*</span>
+                    </label>
+                    <input
+                        onChange={(e) =>
+                            setData({
+                                ...data,
+                                success: false,
+                                error: {},
+                                cPassword: e.target.value,
+                            })
+                        }
+                        value={data.cPassword}
+                        type="password"
+                        id="cPassword"
+                        className={`${
+                            data.error.cPassword ? 'border-red-500' : ''
+                        } px-4 py-2 focus:outline-none border`}
+                    />
+                    {!data.error ? '' : alert(data.error.cPassword, 'red')}
+                </div>
+                <div className="flex flex-col space-y-2 md:flex-row md:justify-between md:items-center">
+                    <div>
+                        <input
+                            type="checkbox"
+                            id="rememberMe"
+                            className="px-4 py-2 mr-1 border focus:outline-none"
+                        />
+                        <label htmlFor="rememberMe">
+                            Remember me
+                            <span className="text-sm text-gray-600">*</span>
+                        </label>
+                    </div>
+                    <a className="block text-gray-600" href="/">
+                        Lost your password?
+                    </a>
+                </div>
+                <div
+                    onClick={(e) => formSubmit()}
+                    style={{ background: '#303031' }}
+                    className="px-4 py-2 font-medium text-center text-white cursor-pointer"
+                >
+                    Create an account
+                </div>
+            </form>
+        </>
+    )
+}
+
+export default Signup
