d10c95aba580d232e278de6ee6e49b7afec99994
Garv <garvwashere@gmail.com>
2021-05-01 16:18:40 +0530

    configured to use tailwind

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+)
 
-serviceWorker.unregister();
+serviceWorker.unregister()
