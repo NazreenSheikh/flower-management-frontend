e7d88266553417209045ccbad7aec204d401492a
Garv <garvwashere@gmail.com>
2021-05-08 15:50:13 +0530

    fixed delete category button

--- src/components/admin/categories/FetchApi.js
+++ src/components/admin/categories/FetchApi.js
@@ -1,74 +1,77 @@
-import axios from "axios";
-const apiURL = process.env.REACT_APP_API_URL;
+import axios from 'axios'
+const apiURL = process.env.REACT_APP_API_URL
 
 const BearerToken = () =>
-  localStorage.getItem("jwt")
-    ? JSON.parse(localStorage.getItem("jwt")).token
-    : false;
+    localStorage.getItem('jwt')
+        ? JSON.parse(localStorage.getItem('jwt')).token
+        : false
 const Headers = () => {
-  return {
-    headers: {
-      token: `Bearer ${BearerToken()}`,
-    },
-  };
-};
+    return {
+        headers: {
+            token: `Bearer ${BearerToken()}`,
+        },
+    }
+}
 
 export const getAllCategory = async () => {
-  try {
-    let res = await axios.get(`${apiURL}/api/category/all-category`, Headers());
-    return res.data;
-  } catch (error) {
-    console.log(error);
-  }
-};
+    try {
+        let res = await axios.get(
+            `${apiURL}/api/category/all-category`,
+            Headers()
+        )
+        return res.data
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 export const createCategory = async ({
-  cName,
-  cImage,
-  cDescription,
-  cStatus,
+    cName,
+    cImage,
+    cDescription,
+    cStatus,
 }) => {
-  let formData = new FormData();
-  formData.append("cImage", cImage);
-  formData.append("cName", cName);
-  formData.append("cDescription", cDescription);
-  formData.append("cStatus", cStatus);
+    let formData = new FormData()
+    formData.append('cImage', cImage)
+    formData.append('cName', cName)
+    formData.append('cDescription', cDescription)
+    formData.append('cStatus', cStatus)
 
-  try {
-    let res = await axios.post(
-      `${apiURL}/api/category/add-category`,
-      formData,
-      Headers()
-    );
-    return res.data;
-  } catch (error) {
-    console.log(error);
-  }
-};
+    try {
+        let res = await axios.post(
+            `${apiURL}/api/category/add-category`,
+            formData,
+            Headers()
+        )
+        return res.data
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-export const editCategory = async (cId, des, status) => {
-  let data = { cId: cId, cDescription: des, cStatus: status };
-  try {
-    let res = await axios.post(
-      `${apiURL}/api/category/edit-category`,
-      data,
-      Headers()
-    );
-    return res.data;
-  } catch (error) {
-    console.log(error);
-  }
-};
+export const editCategory = async (category_id, des, status) => {
+    let data = { category_id: category_id, cDescription: des, cStatus: status }
+    try {
+        let res = await axios.post(
+            `${apiURL}/api/category/edit-category`,
+            data,
+            Headers()
+        )
+        return res.data
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-export const deleteCategory = async (cId) => {
-  try {
-    let res = await axios.post(
-      `${apiURL}/api/category/delete-category`,
-      { cId },
-      Headers()
-    );
-    return res.data;
-  } catch (error) {
-    console.log(error);
-  }
-};
+export const deleteCategory = async (category_id) => {
+    try {
+        let res = await axios.post(
+            `${apiURL}/api/category/delete-category`,
+            { category_id },
+            Headers()
+        )
+        return res.data
+    } catch (error) {
+        console.log(error)
+    }
+}
