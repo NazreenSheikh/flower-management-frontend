2db699b0f35c0bda211dea40473d4eead86c1963
Garv <garvwashere@gmail.com>
2021-05-08 21:48:12 +0530

    removed slider image part

--- src/components/admin/dashboardAdmin/index.js
+++ src/components/admin/dashboardAdmin/index.js
@@ -1,31 +1,30 @@
-import React, { Fragment, createContext, useReducer } from "react";
-import AdminLayout from "../layout";
-import DashboardCard from "./DashboardCard";
-import Customize from "./Customize";
-import { dashboardState, dashboardReducer } from "./DashboardContext";
-import TodaySell from "./TodaySell";
+import React, { createContext, Fragment, useReducer } from 'react'
+import AdminLayout from '../layout'
+import DashboardCard from './DashboardCard'
+import { dashboardReducer, dashboardState } from './DashboardContext'
+import TodaySell from './TodaySell'
 
-export const DashboardContext = createContext();
+export const DashboardContext = createContext()
 
 const DashboardComponent = () => {
-  return (
-    <Fragment>
-      <DashboardCard />
-      <Customize />
-      <TodaySell />
-    </Fragment>
-  );
-};
+    return (
+        <Fragment>
+            <DashboardCard />
+            {/* <Customize /> */}
+            <TodaySell />
+        </Fragment>
+    )
+}
 
 const DashboardAdmin = (props) => {
-  const [data, dispatch] = useReducer(dashboardReducer, dashboardState);
-  return (
-    <Fragment>
-      <DashboardContext.Provider value={{ data, dispatch }}>
-        <AdminLayout children={<DashboardComponent />} />
-      </DashboardContext.Provider>
-    </Fragment>
-  );
-};
+    const [data, dispatch] = useReducer(dashboardReducer, dashboardState)
+    return (
+        <Fragment>
+            <DashboardContext.Provider value={{ data, dispatch }}>
+                <AdminLayout children={<DashboardComponent />} />
+            </DashboardContext.Provider>
+        </Fragment>
+    )
+}
 
-export default DashboardAdmin;
+export default DashboardAdmin
