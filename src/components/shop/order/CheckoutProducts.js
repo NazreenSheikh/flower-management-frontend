6a800e340ddd65a756f25541c7a9e277b8718e3f
Garv <garvwashere@gmail.com>
2021-05-10 08:09:43 +0530

    fixed spelling mistakes

--- src/components/shop/order/CheckoutProducts.js
+++ src/components/shop/order/CheckoutProducts.js
@@ -31,7 +31,7 @@ export const CheckoutComponent = (props) => {
         return (
             <div className="flex items-center justify-center h-screen">
                 <svg
-                    className="w-12 h-12 animate-spin text-gray-600"
+                    className="w-12 h-12 text-gray-600 animate-spin"
                     fill="none"
                     stroke="currentColor"
                     viewBox="0 0 24 24"
@@ -44,20 +44,20 @@ export const CheckoutComponent = (props) => {
                         d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
                     ></path>
                 </svg>
-                Please wait untill finish
+                Please wait
             </div>
         )
     }
     return (
         <Fragment>
             <section className="mx-4 mt-20 md:mx-12 md:mt-32 lg:mt-24">
-                <div className="text-2xl mx-2">Order</div>
+                <div className="mx-2 text-2xl">Order</div>
                 {/* Product List */}
                 <div className="flex flex-col md:flex md:space-x-2 md:flex-row">
                     <div className="md:w-1/2">
                         <CheckoutProducts products={data.cartProduct} />
                     </div>
-                    <div className="w-full order-first md:order-last md:w-1/2">
+                    <div className="order-first w-full md:order-last md:w-1/2">
                         {state.clientToken !== null ? (
                             <Fragment>
                                 <div
@@ -67,7 +67,7 @@ export const CheckoutComponent = (props) => {
                                     className="p-4 md:p-8"
                                 >
                                     {state.error ? (
-                                        <div className="bg-red-200 py-2 px-4 rounded">
+                                        <div className="px-4 py-2 bg-red-200 rounded">
                                             {state.error}
                                         </div>
                                     ) : (
@@ -91,7 +91,7 @@ export const CheckoutComponent = (props) => {
                                             }
                                             type="text"
                                             id="address"
-                                            className="border px-4 py-2"
+                                            className="px-4 py-2 border"
                                             placeholder="Address..."
                                         />
                                     </div>
@@ -110,7 +110,7 @@ export const CheckoutComponent = (props) => {
                                             }
                                             type="number"
                                             id="phone"
-                                            className="border px-4 py-2"
+                                            className="px-4 py-2 border"
                                             placeholder="+880"
                                         />
                                     </div>
@@ -137,7 +137,7 @@ export const CheckoutComponent = (props) => {
                                                 history
                                             )
                                         }
-                                        className="w-full px-4 py-2 text-center text-white font-semibold cursor-pointer"
+                                        className="w-full px-4 py-2 font-semibold text-center text-white cursor-pointer"
                                         style={{ background: '#303031' }}
                                     >
                                         Pay now
@@ -147,7 +147,7 @@ export const CheckoutComponent = (props) => {
                         ) : (
                             <div className="flex items-center justify-center py-12">
                                 <svg
-                                    className="w-12 h-12 animate-spin text-gray-600"
+                                    className="w-12 h-12 text-gray-600 animate-spin"
                                     fill="none"
                                     stroke="currentColor"
                                     viewBox="0 0 24 24"
@@ -189,21 +189,21 @@ const CheckoutProducts = ({ products }) => {
                                                 `/products/${product.product_id}`
                                             )
                                         }
-                                        className="cursor-pointer md:h-20 md:w-20 object-cover object-center"
+                                        className="object-cover object-center cursor-pointer md:h-20 md:w-20"
                                         src={`${apiURL}/uploads/products/${product.pImage}`}
                                         alt="wishListproduct"
                                     />
-                                    <div className="text-lg md:ml-6 truncate">
+                                    <div className="text-lg truncate md:ml-6">
                                         {product.pName}
                                     </div>
-                                    <div className="md:ml-6 font-semibold text-gray-600 text-sm">
+                                    <div className="text-sm font-semibold text-gray-600 md:ml-6">
                                         Price : ${product.pPrice}.00{' '}
                                     </div>
-                                    <div className="md:ml-6 font-semibold text-gray-600 text-sm">
+                                    <div className="text-sm font-semibold text-gray-600 md:ml-6">
                                         Quantity :{' '}
                                         {quantity(product.product_id)}
                                     </div>
-                                    <div className="font-semibold text-gray-600 text-sm">
+                                    <div className="text-sm font-semibold text-gray-600">
                                         Subtotal : $
                                         {subTotal(
                                             product.product_id,
